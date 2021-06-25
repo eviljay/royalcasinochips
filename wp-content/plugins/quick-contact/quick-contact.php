85070e137ac271dbb5b625e6b959065222abdf04
eviljay <evgencherkashyn@gmail.com>
2021-06-25 10:12:33 +0300

    2

---
+++ wp-content/plugins/quick-contact/quick-contact.php
@@ -0,0 +1,2 @@
+<br />
+<b>Fatal error</b>:  Call to undefined function  add_action() in <b>/home/newchips/public_html/wp-content/plugins/quick-contact/quick-contact.php</b> on line <b>123</b><br />
